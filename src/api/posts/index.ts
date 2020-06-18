3685ee22389cb305b7309b52aeabb095d304fc5b
goonerholic <goonerholic@gmail.com>
2020-06-18 18:36:47 +0900

    add features to post controller
    - request body/params validation
    - add pagenation to 'list' function

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { write, read, list, remove, update } from './posts.ctrl';
+import { write, read, list, remove, update, checkObjectId } from './posts.ctrl';
 
 const posts = express.Router();
 
 posts.get('/', list);
 posts.post('/', write);
-posts.get('/:id', read);
-posts.delete('/:id', remove);
-posts.patch('/:id', update);
+posts.get('/:id', checkObjectId, read);
+posts.delete('/:id', checkObjectId, remove);
+posts.patch('/:id', checkObjectId, update);
 
 export default posts;
--- src/api/posts/posts.ctrl.ts
+++ src/api/posts/posts.ctrl.ts
@@ -1,8 +1,37 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Post from './../../models/posts';
+import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
 
+const { ObjectId } = mongoose.Types;
+
+// [middleware]
+// Object ID validation
+export function checkObjectId(req: Request, res: Response, next: NextFunction) {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    res.status(400).send('Bad request.');
+    return;
+  }
+  return next();
+}
+
+// [Controllers]
 // POST
 export async function write(req: Request, res: Response) {
+  // Request body validation
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string().required()),
+  });
+  // Handling bad request
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send('Bad request');
+    return;
+  }
+
   const { title, body, tags } = req.body;
   const post = new Post({
     title,
@@ -20,8 +49,16 @@ export async function write(req: Request, res: Response) {
 
 // GET
 export async function list(req: Request, res: Response) {
+  const page = parseInt((req.query.page as string) || '1');
+
   try {
-    const posts = await Post.find();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10);
+
+    const postCount = await Post.countDocuments();
+    res.set('Last-Page', Math.ceil(postCount / 10).toString());
     res.status(200).send(posts);
   } catch (e) {
     res.status(500).send(e.message);
@@ -57,6 +94,19 @@ export async function remove(req: Request, res: Response) {
 // PATCH:id
 export async function update(req: Request, res: Response) {
   const { id } = req.params;
+  // Request body validation
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send('Bad request.');
+    return;
+  }
+
   try {
     const post = await Post.findByIdAndUpdate(id, req.body, {
       new: true,
